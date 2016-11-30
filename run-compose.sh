dabed903ecef6ab0df2043a875b45edf17f3af69
Sveinn G. Gunnarsson <sveinng@gmail.com>
2016-11-30 20:42:56 +0000

    Use git ls-remote to find git revision - this works everywhere

--- run-compose.sh
+++ run-compose.sh
@@ -4,6 +4,6 @@
 # Compose image from current git revision and start
 #
 
-export GIT_COMMIT=$(git rev-parse HEAD)
+export GIT_COMMIT=$(git ls-remote https://github.com/sveinng/reference-tictactoe HEAD | cut -f 1)
 docker-compose -f docker-compose-env.yaml pull
 docker-compose -f docker-compose-env.yaml up -d
