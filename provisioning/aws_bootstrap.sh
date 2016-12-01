ad363195e396a6e2f9f20d5893c5063a27e859f5
Sveinn G. Gunnarsson <sveinng@gmail.com>
2016-12-01 00:22:35 +0000

    AWS provisioning works. Sudo not needed in bootstrap. Minor fixes

--- provisioning/aws_bootstrap.sh
+++ provisioning/aws_bootstrap.sh
@@ -4,30 +4,38 @@
 #
 
 # Basic update / upgrade + linux extra
-sudo apt-get update
-sudo apt-get upgrade -y
-sudo apt-get install linux-image-extra-$(uname -r) linux-image-extra-virtual -y
+apt-get update
+apt-get upgrade -y
+apt-get install linux-image-extra-$(uname -r) linux-image-extra-virtual -y
 
 # Add support for Docker repository
-sudo apt-get install apt-transport-https ca-certificates
-sudo apt-key adv \
+apt-get install apt-transport-https ca-certificates
+apt-key adv \
                --keyserver hkp://ha.pool.sks-keyservers.net:80 \
                --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
 echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" |  sudo tee /etc/apt/sources.list.d/docker.list
 
 # Install latest docker and start docker service
-sudo apt-get update
-sudo apt-get install docker-engine -y
-sudo service docker start
+apt-get update
+apt-get install docker-engine -y
+service docker start
 
 # Install latest docker compose and make executable
-sudo curl -L "https://github.com/docker/compose/releases/download/1.9.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.9.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 
 # Add ubuntu user to Docker group
-sudo usermod -a -G docker ubuntu
+usermod -a -G docker ubuntu
 
 # Fetch latest docker-compose.yaml and start dockers
-curl -sS https://github.com/sveinng/reference-tictactoe/blob/master/docker-compose-env.yaml
-curl -sS https://github.com/sveinng/reference-tictactoe/blob/master/run-compose.sh
-bash run-compose.sh
+echo "*** Fetching docker-composer scripts"
+cd /home/ubuntu/
+wget https://raw.githubusercontent.com/sveinng/reference-tictactoe/master/docker-compose-env.yaml
+wget https://raw.githubusercontent.com/sveinng/reference-tictactoe/master/run-compose.sh
+
+echo "*** Fixing ownership and permissions"
+chown ubuntu docker-compose-env.yaml run-compose.sh
+chmod +x run-compose.sh
+
+echo "*** Executing run-compose.sh"
+su - ubuntu -c /home/ubuntu/run-compose.sh
--- provisioning/aws_create_instance.sh
+++ provisioning/aws_create_instance.sh
@@ -5,4 +5,4 @@
 #
 
 aws ec2 run-instances --image-id ami-0d77397e --count 1 --instance-type t2.micro --key-name ttt-server \
---user-data $(base64 aws_bootstrap.sh) --subnet-id subnet-2b47105d --security-group-ids sg-81d35ee7
+--subnet-id subnet-2b47105d --security-group-ids sg-81d35ee7 --user-data file://aws_bootstrap.sh
