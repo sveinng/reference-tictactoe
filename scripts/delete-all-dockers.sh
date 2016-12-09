b368b1ad7893291917bcfd460c8ef2ce1be2025e
Sveinn G. Gunnarsson <sveinng@gmail.com>
2016-12-09 16:12:20 +0000

    Only delete tictactoe Docker images from Jenkins server

--- scripts/delete-all-dockers.sh
+++ scripts/delete-all-dockers.sh
@@ -3,8 +3,8 @@
 # Delete all containers
 docker rm $(docker ps -a -q)
 
-# Delete all images
-docker rmi $(docker images -q)
+# Delete all images from tictactoe
+docker rmi $(docker images -q | grep tictactoe)
 
 # Should always return 0 - even if nothing was found to delete
 exit 0
