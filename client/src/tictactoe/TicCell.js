0dfb0de9997915f1545d341a8c6e151e4ed10bb9
Sveinn G. Gunnarsson <sveinng@gmail.com>
2016-12-18 20:11:30 +0000

    Added placeMove to web ui

--- client/src/tictactoe/TicCell.js
+++ client/src/tictactoe/TicCell.js
@@ -9,13 +9,39 @@ export default function (injected) {
         constructor() {
             super();
             this.state = {
+                value: ''
             }
+            this.placeMove = this.placeMove.bind(this);
         }
         componentWillMount(){
+            var moveMade = (cmd)=>{
+                if(cmd.gameId===this.props.gameId) {
+                    if(cmd.coordinates.x===this.props.coordinates.x &&
+                        cmd.coordinates.y===this.props.coordinates.y) {
+                        this.setState({ side: cmd.side});
+                    }
+                }
+            };
+            eventRouter.on('MoveMade', moveMade);
+        }
+        placeMove() {
+            var commandId = generateUUID();
+            var timestamp = new Date();
+
+            var message = {
+                commandId:commandId,
+                type:"PlaceMove",
+                gameId:this.props.gameId,
+                timeStamp:timestamp,
+                side:this.props.mySide,
+                coordinates:this.props.coordinates
+            };
+            commandPort.routeMessage(message);
         }
         render() {
-            return <div className="ticcell">
-                CELL
+            console.debug("this.state", this.props);
+            return <div className="ticcell" onClick={this.placeMove}>
+                {this.state.side}
             </div>
         }
     }
