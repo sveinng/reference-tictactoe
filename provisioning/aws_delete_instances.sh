b4dd5daa69f5681aea33201a5fe95ba7017deac1
Sveinn G. Gunnarsson <sveinng@gmail.com>
2016-12-18 18:09:21 +0000

    AWS delete instances - add test instance

--- provisioning/aws_delete_instances.sh
+++ provisioning/aws_delete_instances.sh
@@ -6,20 +6,22 @@
 
 
 if [[  ! $# -eq 1 ]] ; then
-  printf "\n\t usage $0 <test|prod>\n\n"
-  printf "*** WARNING *** This command deletes all ec2 instances in given environment"
+  printf "\n*** WARNING *** This command deletes all ec2 instances based on the role tag\n"
+  printf "\n\t usage $0 <test|load|prod>\n\n"
+  exit 1
 fi
 
 ENV=$1
 
 # Validate environment
-if [[ $ENV = "test" || $ENV = "prod" ]] ; then
+if [[ $ENV = "test" || $ENV = "prod" || $ENV = "load" ]] ; then
   IDS=$(aws ec2 describe-instances --filters "Name=tag:role,Values=${ENV}" "Name=instance-state-name,Values=running" --query 'Reservations[*].Instances[*].InstanceId')
   for ID in $IDS ; do
-    aws ec2 terminate-instances --instance-ids "$ID" --output text --query 'TerminatingInstances[*].CurrentState.Name'
+    aws ec2 describe-instances  --instance-ids "$ID" --output text --query 'Reservations[0].Instances[0].Tags[0].Value'
+    #aws ec2 terminate-instances --instance-ids "$ID" --output text --query 'TerminatingInstances[*].CurrentState.Name'
   done
 else
   echo "ERR - invalid environment"
-  echo "Valid environments are:  prod / test"
+  echo "Valid environments are:  test | load | prod"
   exit 1
 fi
